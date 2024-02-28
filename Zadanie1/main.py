4674f37403cd3d1cf600be34fcd5c55e427cba43
242424 Kłosowski Hubert <242424@edu.p.lodz.pl>
2024-02-28 17:30:27 +0100

    czesc2 zrobiona

--- Zadanie1/main.py
+++ Zadanie1/main.py
@@ -6,7 +6,7 @@ os.environ[
     "GOOGLE_APPLICATION_CREDENTIALS"] = "projekt1-415610-f19ebf54e064.json"  # lokalizacja pobranego klucza z punktu 1.4
 client = bigquery.Client()
 
-query = 'select * from ´bigquery-public-data.covid19_open_data.covid19_open_data´ limit 10'
+query = 'select * from `bigquery-public-data.covid19_open_data.covid19_open_data` limit 10'
 query_job = client.query(query)
 query_result = query_job.result()
 df = query_result.to_dataframe()
